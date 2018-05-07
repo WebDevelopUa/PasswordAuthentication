be9f1408a72a56ff2d072da85d71c69fbc4b4ab0
WebDev <js.develop.com@gmail.com>
2018-05-07 15:59:33 +0300

    adding project properties

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("Lab")]
+[assembly: AssemblyTitle("Lab")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: ComVisible(true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: Guid("661b5e26-66b6-45a6-9781-ff5291e68edf")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
